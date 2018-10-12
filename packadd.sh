212dd03d328ef0879ec30cb5aadd35a0742b1ffc
Antoine Dray <antoine.dray@epita.fr>
2018-10-12 14:27:54 +0200

    Update packadd.sh

--- packadd.sh
+++ packadd.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
 
+set -e
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-if [ "$#" -ne 1 ]; then
-    printf "usage: packadd [-h | -i | -u | -d]\n"
+info()
+{
+    printf "usage: packadd [-h | -l | -i | -u | -d]\n"
     exit 1
-fi
-
-if [ "$1" = "-h" ]; then
-    clear
-    printf "\nPACKADD(1)      My Commands Manual      PACKADD(1)\n"
-    printf "\nNAME\n"
-    printf "    packadd -- vim package manager\n"
-    printf "\nSYNOPSIS\n"
-    printf "    packadd [argument]\n"
-    printf "\nDESCRIPTION\n"
-    printf "    -h    help\n"
-    printf "    -c    create structure to store packages\n"
-    printf "    -u    update all packages\n"
-    printf "    -i    install a package\n"
-    printf "    -d    delete a package\n\n"
-    exit 0
-fi
+}
 
 init()
 {
@@ -43,7 +30,24 @@ check_init()
     fi
 }
 
-if [ "$1" = "-i" ]; then
+if [ "$#" -ne 1 ]; then
+    info
+elif [ "$1" = "-h" ]; then
+    clear
+    printf "\nPACKADD(1)\t\tMy Commands Manual\t\tPACKADD(1)\n"
+    printf "\nNAME\n\tpackadd -- vim package manager\n"
+    printf "\nSYNOPSIS\n\tpackadd [argument]\n"
+    printf "\nDESCRIPTION\n"
+    printf "\t-h\thelp\n"
+    printf "\t-l\tlist installed packages\n"
+    printf "\t-i\tinstall a package\n"
+    printf "\t-u\tupdate all packages\n"
+    printf "\t-d\tdelete a package\n\n"
+    exit 0
+elif [ "$1" = "-l" ]; then
+    check_init
+    ls ~/.vim/pack/packages/start
+elif [ "$1" = "-i" ]; then
     check_init
     cd ~/.vim
     printf "Enter git url: "
@@ -55,18 +59,14 @@ if [ "$1" = "-i" ]; then
     git commit -m "$name installed"
     printf "${GREEN}${name} installed${NC}\n"
     exit 0
-fi
-
-if [ "$1" = "-u" ]; then
+elif [ "$1" = "-u" ]; then
     check_init
     cd ~/.vim
     git submodule update --remote --merge
     git commit -m "Updated packages" >&- # >&- Hides output of cmd
     printf "${GREEN}Packages updated${NC}\n"
     exit 0
-fi
-
-if [ "$1" = "-d" ]; then
+elif [ "$1" = "-d" ]; then
     check_init
     cd ~/.vim
     echo -ne "Enter package name: "
@@ -77,4 +77,6 @@ if [ "$1" = "-d" ]; then
     git commit -m "${name} uninstalled"
     printf "${GREEN}${name} uninstalled${NC}\n"
     exit 0
+else
+    info
 fi
