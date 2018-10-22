39d33dad1165371c0b95c24823a5dac7a767d00c
cloudnodes <antoine.dray@epita.fr>
2018-10-22 14:00:11 +0200

    Started python implementation

--- packadd.py
+++ packadd.py
@@ -11,6 +11,9 @@ class c:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
+class p:
+    PRE = c.INFO + c.BOLD + '> ' + c.ENDC
+
 vim_dir = '~/.vim'
 vim_packages = '~/.vim/pack/packages'
 
@@ -19,6 +22,16 @@ def init_repo():
         os.makedirs(vim_packages)
     with open(vim_dir + '.gitignore', 'a') as vim:
         vim.write('*\n!pack/packages\n')
-    r = git.Repo.init(vim_packages)
+    repo = git.Repo.init(vim_packages)
+    sub = repo.git.submodule('init')
+    print(p.PRE + 'Packadd initiated')
 
-    print(c.INFO + 'Packadd initiated' + c.END)
+def update():
+    repo = git.Repo.init(vim_packages)
+    sub = repo.git.submodule('update', '--remote', '--merge')
+    repo.commit
+    print(p.PRE + 'Packages updated')
+
+
+def install():
+    repo = git.Repo.clone_from(self._small_repo_url(), os.path.join(rw_dir, 'repo'), branch='master')
