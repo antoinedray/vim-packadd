16dd4c2965edcfde11354f5cc396ae0e4cf1d072
Antoine Dray <antoine.dray@epita.fr>
2018-10-30 15:12:43 +0100

    Update install.sh

--- epita/install.sh
+++ epita/install.sh
@@ -6,7 +6,7 @@ INFO='\033[0;33m'
 NC='\033[0m' # No Color
 
 if [ -f "~/afs/.pip/packadd-fix.sh" ]; then
-    printf "[ ${YELLOW}INFO${NC} ] File already installed\n"
+    printf "[ ${INFO}INFO${NC} ] File already installed\n"
     exit 0
 else
     if [ ! -d "~/afs/.pip" ]; then
@@ -15,18 +15,18 @@ else
 
     chmod u+x packadd-fix.sh
 
-    printf "[ ${YELLOW}INFO${NC} ] Adding patch to .pip folder...\n"
+    printf "[ ${INFO}INFO${NC} ] Adding patch to .pip folder...\n"
 
     mv packadd-fix.sh ~/afs/.pip/
 
-    printf "[ ${YELLOW}INFO${NC} ] Adding alias to your .bashrc\n"
+    printf "[ ${INFO}INFO${NC} ] Adding alias to your .bashrc\n"
 
     echo "# Setup for Vim Packadd, do not remove" >> ~/.bashrc
     echo "alias packadd='/bin/sh ~/afs/.pip/packadd-fix.sh'" >> ~/.bashrc
 
-    printf "[ ${YELLOW}INFO${NC} ] Reloading bashrc\n"
+    printf "[ ${INFO}INFO${NC} ] Reloading bashrc\n"
 
     source ~/.bashrc
 
-    printf "[  ${YELLOW}OK${NC}  ] Installation successfull\n"
+    printf "[  ${OK}OK${NC}  ] Installation successfull\n"
 fi
