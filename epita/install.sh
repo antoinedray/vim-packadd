4085659fdf222ce08ea9a0827b6b1502b066dbdd
Antoine Dray <antoine.dray@epita.fr>
2018-10-30 14:43:05 +0100

    Update install.sh

--- epita/install.sh
+++ epita/install.sh
@@ -5,16 +5,18 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-if [ ! -d "~/.pip" ]; then
-    mkdir ~/.pip
+if [ ! -d "~/afs/.pip" ]; then
+    mkdir ~/afs/.pip
 fi
 
 chmod u+x packadd-fix.sh
 
 printf "[ ${YELLOW}INFO${NC} ] Adding patch to .vim folder...\n"
 
-mv packadd-fix.sh ~/.pip/
+mv packadd-fix.sh ~/afs/.pip/
 
 printf "[ ${YELLOW}INFO${NC} ] Please add the following line to your .bashrc:\n"
 
-printf "\n\talias packadd="/bin/sh ~/.pip/packadd-fix.sh"\n\n"
+printf "\n\talias packadd="/bin/sh ~/afs/.pip/packadd-fix.sh"\n\n"
+
+echo "alias packadd='/bin/sh ~/afs/.pip/packadd-fix.sh'" >> ~/.bashrc
