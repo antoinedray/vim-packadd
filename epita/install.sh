7a4cba5a5fad79253778f37788cb487a34e28982
cloudnodes <antoine.dray@epita.fr>
2018-10-30 12:57:16 +0100

    Updated installation for Epita students

--- epita/install.sh
+++ epita/install.sh
@@ -5,16 +5,16 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-if [ ! -d "~/.vim" ]; then
-    mkdir ~/.vim
+if [ ! -d "~/.pip" ]; then
+    mkdir ~/.pip
 fi
 
 chmod u+x packadd-fix.sh
 
 printf "[ ${YELLOW}INFO${NC} ] Adding patch to .vim folder...\n"
 
-mv packadd-fix.sh ~/.vim/
+mv packadd-fix.sh ~/.pip/
 
 printf "[ ${YELLOW}INFO${NC} ] Please add the following line to your .bashrc:\n"
 
-printf "\n\talias packadd="/bin/sh ~/.vim/packadd-fix.sh"\n\n"
+printf "\n\talias packadd="/bin/sh ~/.pip/packadd-fix.sh"\n\n"
