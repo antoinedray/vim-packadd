475c2e5bdce9cd20a261f1ad73cda52ee2423dbd
cloudnodes <antoine.dray@epita.fr>
2018-10-30 10:20:14 +0100

    Added epita fix patch

---
+++ epita/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+if [ ! -d "~/.vim" ]; then
+    mkdir ~/.vim
+fi
+
+chmod u+x packadd-fix.sh
+
+printf "[ ${YELLOW}INFO${NC} ] Adding patch to .vim folder...\n"
+
+mv packadd-fix.sh ~/.vim/
+
+printf "[ ${YELLOW}INFO${NC} ] Please add the following line to your .bashrc:\n"
+
+printf "\n\talias packadd="/bin/sh ~/.vim/packadd-fix.sh"\n\n"
