b5ab7385ea7f835448fb0880b3fb1a5257a17813
Antoine Dray <antoine.dray@epita.fr>
2019-03-14 20:09:46 +0100

    Uses back packed-runner

--- test/test.sh
+++ test/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-packadd list
-packadd help
-packadd -h
-packadd install https://github.com/morhetz/gruvbox.git
-packadd list
-packadd uninstall gruvbox
+python3 ./packadd-runner.py list
+python3 ./packadd-runner.py help
+python3 ./packadd-runner.py -h
+python3 ./packadd-runner.py install https://github.com/morhetz/gruvbox.git
+python3 ./packadd-runner.py list
+python3 ./packadd-runner.py uninstall gruvbox
