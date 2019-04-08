db00765c6b8dfbe3de15c69a373b8ca05f27020e
Antoine Dray <antoine.dray@epita.fr>
2019-04-08 23:53:36 +0400

    Fixes testfile

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-python3 ./packadd-runner.py help
 python3 ./packadd-runner.py -h
 python3 ./packadd-runner.py install https://github.com/morhetz/gruvbox.git
 python3 ./packadd-runner.py list
