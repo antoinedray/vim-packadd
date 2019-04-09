a1cc368b4d87cf629a4181e60bcac65c4bd92480
Antoine Dray <antoine.dray@epita.fr>
2019-04-09 17:25:47 +0400

    Starts implementing upgrade cmd testing

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ import argparse
 import unittest
 import os
 import sys
+import git
 sys.path.append("..")
 import packadd.packadd as pack
 
@@ -26,16 +27,33 @@ class TestStringMethods(unittest.TestCase):
         self.pi.add_argument('url')
         self.puni = self.subparser.add_parser('uninstall')
         self.puni.add_argument('package')
+        self.plist = self.subparser.add_parser('list')
 
     def test_install(self):
         args = self.pi.parse_args(['https://github.com/morhetz/gruvbox.git'])
         pack.install(args)
         self.assertTrue(os.path.isdir(path.START + '/gruvbox'))
 
+    def test_install_unexisting(self):
+        args = self.pi.parse_args(['https://github.com/morhetz/gruvbo.git'])
+        pack.install(args)
+        self.assertFalse(os.path.isdir(path.START + '/gruvbo'))
+
     def test_uninstall(self):
         args = self.puni.parse_args(['gruvbox'])
         pack.uninstall(args)
         self.assertFalse(os.path.isdir(path.START + '/gruvbox'))
+
+    def test_upgrade(self):
+        args = self.pi.parse_args(['https://github.com/morhetz/gruvbox.git'])
+        repo = git.Repo(path.VIM).submodules[0]
+        # Reset our working tree 10 commits into the past
+        past_branch = repo.create_head('past_branch', 'HEAD~3')
+        repo.head.reference = past_branch
+        assert not repo.head.is_detached
+        # reset the index and working tree to match the pointed-to commit
+        repo.head.reset(index=True, working_tree=True)
+        pack.upgrade(args)
 
 
 def main():
