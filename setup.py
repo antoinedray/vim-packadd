019cc548a56c0f7d407d7524f918a6b038fc2d2f
Antoine Dray <antoine.dray@epita.fr>
2019-05-08 14:40:21 +0200

    Updates setup file

--- setup.py
+++ setup.py
@@ -13,35 +13,32 @@ from epita.command import epita_install
 
 
 version = re.search(
-    '^__version__\s*=\s*"(.*)"',
-    open('packadd/packadd.py').read(),
-    re.M
+        '^__version__\s*=\s*"(.*)"',
+        open('packadd/packadd.py').read(),
+        re.M
     ).group(1)
 
 
-with open("README.md", "r") as fh:
+with open('README.md', "r") as fh:
     long_description = fh.read()
 
 
 setup(
-    name = "vim-packadd",
-    packages = ["packadd"],
-    cmdclass = {
-        'epita_install': epita_install
-    },
-    entry_points = {
-        'console_scripts': ['packadd = packadd.packadd:main'],
-    },
+    name = 'vim-packadd',
     version = version,
-    author = "Antoine Dray",
-    author_email = "antoine.dray@epita.fr",
-    description = "Package manager for Vim8.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license='MIT',
-    install_requires=[
-          'gitpython',
-      ],
-    url = "https://github.com/antoinedray/vim-packadd",
-    test_suite="packadd.tests",
+    author = 'Antoine Dray',
+    author_email = 'antoine.dray@epita.fr',
+    description = 'Package manager for Vim8.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/antoinedray/vim-packadd',
+    license = 'MIT',
+    packages = [ 'packadd' ],
+    cmdclass = { 'epita_install': epita_install },
+    entry_points = {
+        'console_scripts': [ 'packadd = packadd.packadd:main' ],
+    },
+    data_files = [ ('epita', [ '*.sh' ]) ],
+    install_requires = [ 'gitpython' ],
+    test_suite = 'packadd.tests',
 )
