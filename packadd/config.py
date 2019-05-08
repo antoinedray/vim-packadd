ea887aa46eef04a4fb5870defc414c277c1f2594
Antoine Dray <antoine.dray@epita.fr>
2019-05-08 20:27:24 +0200

    Fixes classes order

--- packadd/config.py
+++ packadd/config.py
@@ -9,35 +9,28 @@ import site
 import platform
 
 
-class Aliases:
-    COMMENT = '\n# Setup for Vim Packadd, do not remove\n'
-    LINKSCRIPT = "alias packadd='/bin/sh ~/afs/.pip/packadd.sh'"
-    PY_BIN = 'export PATH=' + Paths.BIN + ':$PATH'
-    FULL = COMMENT + LINKSCRIPT + '\n' + PY_BIN + '\n'
-
-
 class Colors:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+    FAIL = '\033[31m'
     HEADER = '\033[95m'
     INFO = '\033[94m'
     OK = '\033[92m'
-    WARN = '\033[93m'
-    FAIL = '\033[31m'
-    BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-    END = '\033[0m'
+    WARN = '\033[93m'
 
 
 class Paths:
     BASHFILE = ('.bash_profile', '.bashrc')[platform.system() == 'Linux']
     BASHRC = os.environ['HOME'] + '/' + BASHFILE
+    BIN = site.USER_BASE + '/bin'
+    CONF_VIM = os.environ['HOME'] + '/afs/.confs/.vim'
+    INSTALL_SH = os.environ['HOME'] + '/afs/.confs/install.sh'
     PATCH = os.environ['HOME'] + '/afs/.pip/packadd.sh'
     PIP = os.environ['HOME'] + '/afs/.pip'
-    VIM = os.environ['HOME'] + '/.vim'
-    CONF_VIM = os.environ['HOME'] + '/afs/.confs/.vim'
     START = os.environ['HOME'] + '/.vim/pack/packadd/start/'
     OPT = os.environ['HOME'] + '/.vim/pack/packadd/opt/'
-    BIN = site.USER_BASE + '/bin'
-    INSTALL_SH = os.environ['HOME'] + '/afs/.confs/install.sh'
+    VIM = os.environ['HOME'] + '/.vim'
 
 
 class Prints:
@@ -48,3 +41,10 @@ class Prints:
     PRE_OK = Colors.OK + Colors.BOLD + '> ' + Colors.END
     PRE_OK_L = Colors.OK + Colors.BOLD + '==> ' + Colors.END
     PRE_LIST = Colors.INFO + Colors.BOLD + '  - ' + Colors.END
+
+
+class Aliases:
+    COMMENT = '\n# Setup for Vim Packadd, do not remove\n'
+    LINKSCRIPT = "alias packadd='/bin/sh ~/afs/.pip/packadd.sh'"
+    PY_BIN = 'export PATH=' + Paths.BIN + ':$PATH'
+    FULL = COMMENT + LINKSCRIPT + '\n' + PY_BIN + '\n'
