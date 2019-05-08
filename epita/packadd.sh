7e87b755d39a6cd733ddfd8ce0decb840b5adac7
Antoine Dray <antoine.dray@epita.fr>
2019-05-08 15:07:14 +0200

    Updates prints in packadd.sh

--- epita/packadd.sh
+++ epita/packadd.sh
@@ -4,13 +4,14 @@
 python3 -m site &> /dev/null && PATH="$PATH:`python3 -m site --user-base`/bin"
 
 command_exists() {
+    echo "$1"
     type "$1" &> /dev/null ;
 }
 
 packadd_fix() {
     if ! command_exists packadd; then
-        $(pip install --user vim-packadd)
         echo "packdd does not yet exists"
+        $(pip install --user vim-packadd)
     fi
 }
 
